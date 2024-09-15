46a87fffa75f1c66ad944ff81e475029085948ee
Keshav Madhav <keshav2552003@gmail.com>
2024-09-15 17:15:27 +0530

    Added backend logic and api for login and setting user in convex db

---
+++ convex/user.ts
@@ -0,0 +1,24 @@
+import { internalQuery, internalMutation } from "./_generated/server";
+import {v} from 'convex/values'
+
+export const create = internalMutation({
+  args:{
+    username: v.string(),
+    status: v.string(),
+    email: v.string(),
+    imageUrl: v.string(),
+    clerkId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return ctx.db.insert("users", args)
+  }
+})
+
+export const get = internalQuery({
+  args:{
+    clerkId: v.string(),
+  },
+  async handler(ctx, args){
+    return ctx.db.query('users').withIndex('by_clerkId', q => q.eq("clerkId", args.clerkId)).unique()
+  }
+})
